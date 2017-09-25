cd7b424a2709a5bbccb5d67239623ee4365a893a
Michał Gacek <gacek.michael@outlook.com>
2017-09-25 02:51:23 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, hashHistory} from 'react-router';
 import { Provider } from 'react-redux';
+
 import store from './store/store';
-import DevTools from './DevTools';
 import { getCountries } from './actions/actions-countries';
 import routes from './routes';
+import AppContainer from './containers/app-container.component';
 
 render(	
 		<Provider store={store}>
-			<Router history={hashHistory} routes={routes}/>
-			
+			<AppContainer routes={ routes } />
 		</Provider>,
 	document.getElementById('root')
 
